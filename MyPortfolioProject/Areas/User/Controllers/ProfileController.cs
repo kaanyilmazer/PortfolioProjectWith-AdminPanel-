c748ad55cc03fa3459d6c0385d91b0e5f9efd487
kaan <iamyilmazer@gmail.com>
2022-12-07 00:54:45 +0300

    Profile Page Added

---
+++ MyPortfolioProject/Areas/User/Controllers/ProfileController.cs
@@ -0,0 +1,53 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyPortfolioProject.Areas.User.Models;
+
+namespace MyPortfolioProject.Areas.User.Controllers
+{
+    [Area("User")]
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<AspUser> _userManager;
+
+        public ProfileController(UserManager<AspUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel model = new UserEditViewModel();
+            model.Name = values.Name;
+            model.Surname = values.Surname;
+            model.PictureURL = values.ImageUrl;
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel p)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (p.Picture != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(p.Picture.FileName);
+                var imagename = Guid.NewGuid() + extension;
+                var savelocation = resource + "/wwwroot/UserImage/"+ imagename;
+                var stream = new FileStream(savelocation, FileMode.Create);
+                await p.Picture.CopyToAsync(stream);
+                user.ImageUrl = imagename;
+            }
+            user.Name = p.Name;
+            user.Surname = p.Surname;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
+        }
+    }
+}
---
+++ MyPortfolioProject/Areas/User/Models/UserEditViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MyPortfolioProject.Areas.User.Models
+{
+    public class UserEditViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+        public string PictureURL { get; set; }
+        public IFormFile Picture { get; set; }
+    }
+}
