cd16c1c519ab55425a83bd4fe86e98eb69beff74
kaan <iamyilmazer@gmail.com>
2023-04-04 22:30:01 +0300

    Admin ve Kullanıcı Panel Düzenlemeleri Tamamlandı

--- MyPortfolioProject/ViewComponents/Dashboard/MessageList.cs
+++ MyPortfolioProject/ViewComponents/Dashboard/MessageList.cs
@@ -8,16 +8,18 @@ namespace MyPortfolioProject.ViewComponents.Dashboard
     public class MessageList : ViewComponent
     {
      Context  c = new Context();
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
        
         public IViewComponentResult Invoke()
         {
-           
-            
+
+
 
             //ViewBag.v1 = c.UserMessages.OrderByDescending(x => x.Date)
             //                .Select(y => y.Date)
             //                .First().ToShortDateString();
-            return View();
+            var values = messageManager.TGetList();
+            return View(values);
         }
     }
 }
